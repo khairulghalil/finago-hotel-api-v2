0ab1498bf4b8dd3507dbfbf418b2fdd2d7fe50e6
Khairul Ghalil <khairulghalil@Khairul-MacBook.local>
2026-05-26 19:52:21 +0800

    CORS Fix

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,5 +4,5 @@ export default registerAs('app', () => ({
   nodeEnv: process.env.NODE_ENV || 'development',
   port: parseInt(process.env.PORT || '3000', 10),
   apiPrefix: process.env.API_PREFIX || 'api/v1',
-  corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  corsOrigin: process.env.CORS_ORIGIN === 'true' ? true : (process.env.CORS_ORIGIN || 'http://localhost:3000'),
 }));
